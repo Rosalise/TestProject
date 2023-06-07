5e6617fe7bcf2300b98e27c80bf9b90d938718dc
s2200691 <s2200691@edu.bc.fi>
2023-06-07 14:04:09 +0300

    initial commit

---
+++ TaskProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            WriteTime(DateTime.Now);
+        }
+
+        public static void WriteTime(DateTime moment, string filePath = null)
+        {
+            if (filePath == null)
+            {
+                filePath = Path.GetTempFileName();
+            }
+            File.WriteAllText(filePath, moment.ToString());
+
+        }
+    }
+}
---
+++ TestTaskProject/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TaskProject;
+using System.IO;
+
+namespace TestTaskProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFileCreation()
+        {
+            var fileName = "datetime_data.txt";
+            Program.WriteTime(DateTime.Now, fileName);
+
+            Assert.IsTrue(File.Exists(fileName));
+        }
+
+        [TestMethod]
+        public void TestFileNotEmpty()
+        {
+            var fileName = "datetime_data.txt";
+            Program.WriteTime(DateTime.Now, fileName);
+
+            var testData = File.ReadAllText(fileName);
+            Assert.IsTrue(testData.Length > 0);
+        }
+
+
+        [TestMethod]
+        public void TestTimeCorrectness()
+        {
+            var fileName = "datetime_data.txt";
+            Program.WriteTime(DateTime.Now, fileName);
+
+            var testData = File.ReadAllText(fileName);
+
+            Assert.AreEqual(testData, DateTime.Now.ToString(), "Dates are not equal");
+        }
+    }
+}
